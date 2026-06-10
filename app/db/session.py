eca66a244bf95be3f64cea37178b0678fac11211
weiweijason <wegos9012@gmail.com>
2026-06-10 18:05:22 +0800

    fix engine size

--- app/db/session.py
+++ app/db/session.py
@@ -6,8 +6,8 @@ from app.core.config import DATABASE_URL
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
-    pool_size=20,
-    max_overflow=30,
+    pool_size=50,
+    max_overflow=100,
     pool_timeout=30,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
