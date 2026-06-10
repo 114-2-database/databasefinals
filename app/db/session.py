42c57730cb70bf7dabdba0706bf1481e89b6dd66
weiweijason <wegos9012@gmail.com>
2026-06-10 17:47:35 +0800

    fix engine size

--- app/db/session.py
+++ app/db/session.py
@@ -3,6 +3,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=20,
+    max_overflow=30,
+    pool_timeout=30,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
